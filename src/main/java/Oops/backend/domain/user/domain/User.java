ef7b39bfdd684a0acfd46eaf21179e9489c3a20f
GahBaek <gusqorrk@gmail.com>
2025-07-07 22:10:31 +0900

    feat: User Entity에 이름, 이메일, 비밀번호 추각 - #1

--- src/main/java/Oops/backend/domain/user/domain/User.java
+++ src/main/java/Oops/backend/domain/user/domain/User.java
@@ -1,2 +1,15 @@
-package Oops.backend.domain.user.domain;public class User {
+package Oops.backend.domain.user.domain;
+
+import Oops.backend.domain.common.BaseEntity;
+import jakarta.persistence.Column;
+
+public class User extends BaseEntity {
+    @Column(length = 100, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
 }
