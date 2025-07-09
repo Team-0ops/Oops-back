f8d3fc3c15807cabb966fe908c1ee3c950d46107
GahBaek <gusqorrk@gmail.com>
2025-07-09 21:50:23 +0900

    feat: Join 비즈니스 로직 작성 - #1

--- src/main/java/Oops/backend/auth/repository/AuthRepository.java
+++ src/main/java/Oops/backend/auth/repository/AuthRepository.java
@@ -1,2 +1,9 @@
-package Oops.backend.auth.repository;public class AuthRepository {
+package Oops.backend.auth.repository;
+
+import Oops.backend.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AuthRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
--- src/main/java/Oops/backend/auth/service/AuthService.java
+++ src/main/java/Oops/backend/auth/service/AuthService.java
@@ -1,2 +1,46 @@
-package Oops.backend.auth.service;public class AuthService {
+package Oops.backend.auth.service;
+
+import Oops.backend.auth.PasswordHashEncryption;
+import Oops.backend.auth.dto.request.JoinDto;
+import Oops.backend.common.exception.GeneralException;
+import Oops.backend.domain.user.domain.User;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import Oops.backend.auth.repository.AuthRepository;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+    private final AuthRepository authRepository;
+    private final PasswordHashEncryption passwordHashEncryption;
+
+    /*
+    회원가입
+     */
+    public void join(JoinDto joinDto, HttpServletResponse response) {
+        // 이메일이 이미 존재하는지 확인
+        this.isEmailExist(joinDto.getEmail());
+        String encryptedPassword = this.passwordHashEncryption.encrypt(joinDto.getPassword());
+
+        // 이메일이 존재하지 않는다면 새로운 User 생성
+        User user = User.builder()
+                .email(joinDto.getEmail())
+                .password(encryptedPassword)
+                .username(joinDto.getUsername())
+                .build();
+
+        authRepository.save(user);
+    }
+
+    /*
+    Email 유일성 확인
+     */
+    public void isEmailExist(String email) {
+        User user = this.authRepository.findByEmail(email);
+        if (user != null) {
+            throw new RuntimeException("이미 존재하는 이메일 입니다.");
+        }
+    }
+
 }
