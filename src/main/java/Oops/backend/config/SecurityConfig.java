1846f666a41ed3ac4eaf5bb55d858cfb8ce3bbb2
Da Young Kim <dayoungann@naver.com>
2025-07-18 16:28:29 +0900

    ✨ [FEAT] #11 마이페이지 AP

--- src/main/java/Oops/backend/config/SecurityConfig.java
+++ src/main/java/Oops/backend/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/posts/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/posts/**","/api/my-page/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(config -> config.disable())
