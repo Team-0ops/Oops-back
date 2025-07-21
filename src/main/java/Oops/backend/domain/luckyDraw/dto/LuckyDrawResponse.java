9fdeb752712f84cabdb9e38f2acf9fc56177b87d
hyerin <rin4219@naver.com>
2025-07-22 02:24:55 +0900

    lucky draw ê´API

---
+++ src/main/java/Oops/backend/domain/luckyDraw/dto/LuckyDrawResponse.java
@@ -0,0 +1,30 @@
+package Oops.backend.domain.luckyDraw.dto;
+
+import Oops.backend.domain.luckyDraw.entity.LuckyDraw;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class LuckyDrawResponse {
+    /**
+     * 행운부적 추첨 결과
+     */
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LuckyDrawResponseDto {
+        String name;
+        String content;
+        String imageUrl;
+
+        public static LuckyDrawResponse.LuckyDrawResponseDto from(LuckyDraw luckyDraw) {
+            return LuckyDrawResponse.LuckyDrawResponseDto.builder()
+                    .name(luckyDraw.getName())
+                    .content(luckyDraw.getContent())
+                    .imageUrl(luckyDraw.getImageUrl())
+                    .build();
+        }
+    }
+}
