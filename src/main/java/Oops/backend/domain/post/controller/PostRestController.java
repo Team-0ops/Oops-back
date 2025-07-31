0cb2344692d0808a2a903e437afc1553ba1ccba0
Da Young Kim <dayoungann@naver.com>
2025-07-31 22:57:54 +0900

    🐛 [BUG]#116 develop 브랜치 커밋 기록 수정

--- src/main/java/Oops/backend/domain/post/controller/PostRestController.java
+++ src/main/java/Oops/backend/domain/post/controller/PostRestController.java
@@ -4,12 +4,10 @@ package Oops.backend.domain.post.controller;
 import Oops.backend.common.response.BaseResponse;
 import Oops.backend.common.status.SuccessStatus;
 import Oops.backend.domain.auth.AuthenticatedUser;
-import Oops.backend.domain.post.dto.PostCreateRequest;
-import Oops.backend.domain.post.dto.PostCreateResponse;
-import Oops.backend.domain.post.dto.PostRecommendationResponse;
-import Oops.backend.domain.post.dto.PostSummaryDto;
+import Oops.backend.domain.post.dto.*;
 import Oops.backend.domain.post.model.Situation;
 import Oops.backend.domain.post.service.PostCommandService;
+import Oops.backend.domain.post.service.PostQueryService;
 import Oops.backend.domain.post.service.PostRecommendationQueryService;
 import Oops.backend.domain.user.entity.User;
 import jakarta.validation.Valid;
@@ -27,6 +25,7 @@ public class PostRestController {
 
     private final PostCommandService postCommandService;
     private final PostRecommendationQueryService postRecommendationQueryService;
+    private final PostQueryService postQueryService;
 
     @PostMapping("/{postId}/cheers")
     public ResponseEntity<BaseResponse> postCheer(@PathVariable Long postId,
@@ -44,6 +43,14 @@ public class PostRestController {
         PostCreateResponse response = postCommandService.createPost(user, request);
         return BaseResponse.onSuccess(SuccessStatus._CREATED, response);
     }
+    // [추가] 내가 작성한 전체 실패담 조회 API
+    @GetMapping("/my")
+    public ResponseEntity<BaseResponse> getMyPosts(@AuthenticatedUser User user) {
+        List<PostDetailSummaryDto> response = postQueryService.getMyPosts(user);
+        return BaseResponse.onSuccess(SuccessStatus._OK, response);
+    }
+
+    /*
     // 상황에 따라 연결 가능한 이전 글 목록 조회
     @GetMapping("/previous")
     public ResponseEntity<BaseResponse> getPreviousPostsForSituation(
@@ -63,6 +70,7 @@ public class PostRestController {
 
         return BaseResponse.onSuccess(SuccessStatus._OK, result);
     }
+*/
 
     //실패담 추천
     // PostRestController.java
