1113773617ef7140c49ab6c426e6628bc9fc304f
GahBaek <gusqorrk@gmail.com>
2025-08-04 23:28:25 +0900

    bug 설정 - #177

--- src/main/java/Oops/backend/config/SecurityConfig.java
+++ src/main/java/Oops/backend/config/SecurityConfig.java
@@ -23,8 +23,9 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/swagger-ui/**",
+                        .requestMatchers("/api/auth/login", "/api/auth/login", "/api/auth/reset-password", "/swagger-ui/**",
                                 "/v3/api-docs/**", "/api/posts/**","/api/my-page/**",
+                                "/api/posts/my",
                                 "/api/categories/**", "/api/feeds/**",
                                 "/api/lucky-draw", "/api/**")
                         .permitAll()
