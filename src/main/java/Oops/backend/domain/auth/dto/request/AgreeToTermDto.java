327f3caf23961235b77bc83f454275e50f09c92a
GahBaek <gusqorrk@gmail.com>
2025-08-10 01:06:30 +0900

    feat: 회원가입 로직 변경 - #256

--- src/main/java/Oops/backend/domain/auth/dto/request/AgreeToTermDto.java
+++ src/main/java/Oops/backend/domain/auth/dto/request/AgreeToTermDto.java
@@ -2,13 +2,16 @@ package Oops.backend.domain.auth.dto.request;
 
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
 import java.util.UUID;
 
+@Getter
 public class AgreeToTermDto {
-    @NotBlank(message = "약관 id가 비었습니다.")
-    private UUID termId;
+    @NotNull(message = "약관 id가 비었습니다.")
+    private Long termId;
 
-    @AssertTrue
+    @NotNull(message = "동의 여부가 비었습니다.")
     private boolean agreed;
 }
--- src/main/java/Oops/backend/domain/auth/dto/request/JoinDto.java
+++ src/main/java/Oops/backend/domain/auth/dto/request/JoinDto.java
@@ -17,7 +17,7 @@ public class JoinDto {
 
     @Schema(description = "사용자 이메일", example = "test@example.com")
     @NotBlank(message = "이메일이 비었습니다.")
-    @Pattern(regexp = "^[A-Za-z0-9]+@[A-Za-z0-9]+\\.[A-Za-z]{2,6}$", message = "이메일 형식이 맞지 않습니다.")
+    @jakarta.validation.constraints.Email(message = "이메일 형식이 맞지 않습니다.")
     private String email;
 
     @Schema(description = "사용자 닉네임", example = "홍길동")
@@ -25,11 +25,11 @@ public class JoinDto {
     private String userName;
 
     @Schema(description = "사용자 비밀번호", example = "1234abcd!")
-    @NotBlank(message = "비밀번호기 비어있습니다.")
+    @NotBlank(message = "비밀번호가 비어있습니다.")
     private String password;
-
 
     @Schema(description = "사용자 약관 동의 목록")
     @NotNull(message = "약관 동의 정보가 필요합니다.")
+    @jakarta.validation.Valid
     private List<AgreeToTermDto> termsAgreement;
 }
