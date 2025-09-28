8ddf195bccdc0a6ea76f21bb0f6da87c0efffa76
GahBaek <gusqorrk@gmail.com>
2025-09-28 15:31:06 +0900

    feat: SocialAccount entity 생성 - #396

--- src/main/java/Oops/backend/domain/auth/entity/SocialAccount.java
+++ src/main/java/Oops/backend/domain/auth/entity/SocialAccount.java
@@ -1,2 +1,30 @@
-package Oops.backend.domain.auth.entity;public class SocialAccount {
+package Oops.backend.domain.auth.entity;
+
+import Oops.backend.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "social_accounts",
+        uniqueConstraints = @UniqueConstraint(name="ux_social_provider",
+                columnNames={"provider","providerId"}))
+public class SocialAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(nullable=false, length=32)
+    private String provider;
+
+    @Column(nullable=false, length=128)
+    private String providerId;
+
+    @Column(length=255)
+    private String emailFromProvider;
 }
