82d94af51572556ac9df1ec9a5c05653ec88ea31
llokr1 <ghdwlsrl100@gachon.ac.kr>
2026-03-09 22:58:34 +0900

    bug: 신고 유효성 검증 로직 추가

--- src/main/java/Oops/backend/domain/postReport/repository/PostReportRepository.java
+++ src/main/java/Oops/backend/domain/postReport/repository/PostReportRepository.java
@@ -1,9 +1,11 @@
 package Oops.backend.domain.postReport.repository;
 
+import Oops.backend.domain.post.entity.Post;
 import Oops.backend.domain.postReport.entity.PostReport;
 import Oops.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostReportRepository extends JpaRepository<PostReport, Long> {
     long countByUser(User user);
+    boolean existsByUserAndPost(User user, Post post);
 }
